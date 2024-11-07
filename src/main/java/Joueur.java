4e2d1ce80a97bc4c572426734a05719c7f62eb23
Cartier Lou <lou.cartier09@gmail.com>
2024-11-07 17:02:48 +0100

    j'ai fini les jeux atteindre 10 et atteindre 15

--- src/main/java/Joueur.java
+++ src/main/java/Joueur.java
@@ -4,5 +4,7 @@ public class Joueur{
     public void remplirGrille(int ligneMeilleureCase, int colonneMeilleureCase, int chiffre, Grille g){
         g.grille[ligneMeilleureCase][colonneMeilleureCase]=chiffre;
     }
+
+    
     
 }
