b8571c2d4fae77d582df4fd2fb448d1bf37fac71
atandabany <tandabany.adrien@hotmail.fr>
2024-11-13 22:22:41 +0100

    mini jeu echelle de trois

--- src/main/java/MiniJeux.java
+++ src/main/java/MiniJeux.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class MiniJeux {
 
     public static void main(String ...args){
-        AtteindreX sommeDe15 = new AtteindreX(15);
+        //AtteindreX sommeDe15 = new AtteindreX(15);
         //AtteindreX atteindre10 = new AtteindreX(10);
+        EchelleDeTrois echelleDeTrois = new EchelleDeTrois();
     }
 
 
